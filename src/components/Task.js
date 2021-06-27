c17bbeb46d9cb0e59c0af1dffae9fca67af2e52e
monibix <monibixv2@gmail.com>
2021-06-27 09:40:22 +0200

    styled with material-ui

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import "./Task.css"
+import "./Task.css";
+import { Paper, Typography, TextField, Button, List, ListItem, ListItemText, ListItemSecondaryAction,  IconButton } from '@material-ui/core'
+import Delete from '@material-ui/icons/Delete';
+import DoneOutline from '@material-ui/icons/DoneOutline';
+
 
 let id = 0;
 
@@ -60,25 +64,27 @@ function Task() {
     console.log("tasklist", taskList)
     console.log("tasklist", taskList.length)
     return(
-        <div>
-            <h1>Task App</h1>
-            <p>You have {counter()} uncompleted and {taskList.length} total tasks</p>
+        <div className="title">
+            <Typography variant='h2' align='center' gutterBottom>📝 Task App</Typography>
+            <Typography variant='h5' align='center' gutterBottom>You have {counter()} uncompleted and {taskList.length} total tasks</Typography>
             <div>
                 {
                     taskList.map((task, index)=>{
-                        return <div key={index} className={toggleActiveStyles(index)}> 
+                        return <List key={index} className={toggleActiveStyles(index)}> 
                             <p>{task.title}</p>
                             <div>
-                                <button onClick={()=> toggleActive(index)}>Done</button>
-                                <button onClick={()=>handleDelete(task.id)}>Delete Task</button>
+                                <IconButton><Button variant="contained" color="primary" onClick={()=> toggleActive(index)}><DoneOutline/></Button></IconButton>
+                                <IconButton><Button variant="contained" color="primary" onClick={()=>handleDelete(task.id)}><Delete/></Button></IconButton>
                             </div>
-                        </div>
+                        </List>
                     })
                 }
             </div>
 
             <div>
+                <Paper>
                 <form onSubmit={handleSubmit}>
+                <TextField>
                     <input 
                         type="text"
                         name="title" 
@@ -87,8 +93,10 @@ function Task() {
                         minLength="1" 
                         required 
                     />
-                    <button>Add Task</button>
+                </TextField>
+                    <Button variant="contained" color="primary">Add Task</Button>
                 </form>
+                </Paper>
             </div>
 
         </div>
